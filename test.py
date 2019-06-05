74057680bf2caddced1a688554a58a6a5237a02b
CandyNinja <candyninja001@gmail.com>
2019-06-04 22:57:01 -0700

    Removed ignored files

--- test.py
+++
@@ -1,7 +0,0 @@
-from pypad.game import Game
-from pypad.awakening import Awakening
-from pypad.collab import Collab
-from pypad.skill_loader import SkillLoader
-from collections import defaultdict
-
-game = Game()
