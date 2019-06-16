e551c2cf46fe1aa84de00536e814ce48112daf64
CandyNinja <candyninja001@gmail.com>
2019-06-16 14:46:30 -0700

    Fixed ranking score time modifier

--- pypad/dungeon/floor_modifier/ranking_score_time_modifier_fm.py
+++ pypad/dungeon/floor_modifier/ranking_score_time_modifier_fm.py
@@ -6,15 +6,15 @@ class RankingScoreTimeModifierFM(FloorModifier):
         return name == 'rsmtm'
 
     def parse_value(self):
-        self.time_score_modifier = int(self.value) / 100
+        self.score_modifier = int(self.value) / 100
 
     def args_to_json(self) -> dict:
         return {
-            'time_score_modifier': self.time_score_modifier,
+            'score_modifier': self.score_modifier,
         }
 
     def localize(self) -> str:
-        return '{self.time_score_modifier}x score from remaining time'
+        return f'{self.score_modifier}x score from remaining time'
 
     @property
     def floor_modifier_type(self):
