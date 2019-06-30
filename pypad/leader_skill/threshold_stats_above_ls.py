386ae8cdccffc5884b45b6a941e218ae07c73d15
CandyNinja <candyninja001@gmail.com>
2019-06-30 10:53:22 -0700

    Fixed bug in parsing

--- pypad/leader_skill/threshold_stats_above_ls.py
+++ pypad/leader_skill/threshold_stats_above_ls.py
@@ -18,31 +18,31 @@ class ThresholdStatsAboveLS(LeaderSkill):
         self.reduction_attributes = all_attr
         self.damage_reduction = 0.0
 
-        if self.internal_skill_type == 38:
+        if self.internal_skill_type == 43:
             self.damage_reduction = self.args[2] / 100
 
-        elif self.internal_skill_type == 39:
+        elif self.internal_skill_type == 44:
             self.atk_multiplier = self.args[3] if 1 in self.args[1:3] else 1.0
             self.rcv_multiplier = self.args[3] if 2 in self.args[1:3] else 1.0
 
-        elif self.internal_skill_type == 94:
+        elif self.internal_skill_type == 96:
             self.for_attributes = (AttackAttribute(self.args[1]),)
             self.atk_multiplier = self.args[4] if 1 in self.args[2:4] else 1.0
             self.rcv_multiplier = self.args[4] if 2 in self.args[2:4] else 1.0
 
-        elif self.internal_skill_type == 95:
+        elif self.internal_skill_type == 97:
             self.for_attributes = tuple()
             self.for_types = (MonsterType(self.args[1]),)
             self.atk_multiplier = self.args[4] if 1 in self.args[2:4] else 1.0
             self.rcv_multiplier = self.args[4] if 2 in self.args[2:4] else 1.0
 
-        elif self.internal_skill_type == 122:
+        elif self.internal_skill_type == 123:
             self.for_attributes = tuple(AttackAttribute(a) for a in binary_to_list(self.args[1]))
             self.for_types = tuple(MonsterType(t) for t in binary_to_list(self.args[2]))
             self.atk_multiplier = optional_multiplier(self.args[3])
             self.rcv_multiplier = optional_multiplier(self.args[4])
 
-        elif self.internal_skill_type == 130:
+        elif self.internal_skill_type == 131:
             self.for_attributes = tuple(AttackAttribute(a) for a in binary_to_list(self.args[1]))
             self.for_types = tuple(MonsterType(t) for t in binary_to_list(self.args[2]))
             self.atk_multiplier = optional_multiplier(self.args[3])
